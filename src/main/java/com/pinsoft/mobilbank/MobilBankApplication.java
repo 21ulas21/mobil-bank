00f06d19900ed0eb569d696604c26ea118579545
21ulas21 <ulasegrk21@hotmail.com>
2023-09-06 17:35:04 +0300

    first
    first

---
+++ src/main/java/com/pinsoft/mobilbank/MobilBankApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.mobilbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinsoft/mobilbank/MobilBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinsoft.mobilbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
